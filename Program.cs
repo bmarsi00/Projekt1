08b938bfc6b67290b81b7141131e0d37f12bef29
ancovic <antcov0101@gmai.com>
2025-03-17 17:13:57 +0100

    aaa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+namespace Projekt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Novo");
+        }
+    }
+}
